78973ad7ed71efbe0f7080b5b57c0b02418055ca
bULLETHELL <polyswing7@hotmail.com>
2025-10-31 20:55:20 +0100

    gruvbox

--- .config/nvim/lua/lazy-plugins.lua
+++ .config/nvim/lua/lazy-plugins.lua
@@ -7,6 +7,7 @@ require('lazy').setup {
 	-- keys can be used to configure plugin behavior/loading/etc.
 	--
 	-- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
+	require 'standard.plugins.colorscheme',
 	require 'standard.plugins.lspconfig',
 	require 'standard.plugins.blink-cmp',
 	require 'standard.plugins.autopairs',
